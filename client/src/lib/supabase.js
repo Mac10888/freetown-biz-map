bd9413daa9b52018238dab3b3422e65a3c46fa57
Mac10888 <mndongtoelawfirm@gmail.com>
2026-02-06 00:18:07 +0100

    update to use actual Supabase URL

--- client/src/lib/supabase.js
+++ client/src/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(
-  'YOUR_SUPABASE_URL',
-  'YOUR_SUPABASE_ANON_KEY'
+  'https://eukfjmahrtrslixolgah.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1a2ZqbWFocnRyc2xpeG9sZ2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMzIyMDMsImV4cCI6MjA4NTkwODIwM30.VSQu087japuf1_3LhFxgVFenWAPVabS42aiSqqEXM5Q'
 );
